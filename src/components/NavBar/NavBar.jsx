73c053a2f2493e5602d825b7bddaeb73b592e2a3
Doris Li <dorislxy@gmail.com>
2021-12-06 09:37:00 -0800

    nav bar css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,8 +12,30 @@ export default function NavBar(props) {
 
   return (
     <>
-    <h1>Secret Santa</h1>
-    <nav>
+      <header class="p-3 mb-3 border-bottom">
+        <div class="container">
+          <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+              <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <li><a href="#" class="nav-link px-2 link-secondary">Overview</a></li>
+              <li><a href="#" class="nav-link px-2 link-dark">Inventory</a></li>
+              <li><a href="#" class="nav-link px-2 link-dark">Customers</a></li>
+              <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>
+            </ul>
+{/* 
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+              <input type="search" class="form-control" placeholder="Search..." aria-label="Search" />
+            </form> */}
+
+  
+          </div>
+        </div>
+      </header>
+      <h1>Secret Santa</h1>
+    {/* <nav>
       <Link to="/">Home</Link>
       &nbsp; | &nbsp;
       <Link to="/search">Search</Link>
@@ -25,7 +47,7 @@ export default function NavBar(props) {
       <span>Welcome, collect and share what you like {props.user.name}</span>
       &nbsp; | &nbsp;
       <Link to="" onClick={handleLogOut}>Log Out</Link>
-    </nav>
+    </nav> */}
     </>
   );
 }
